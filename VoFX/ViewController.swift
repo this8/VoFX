bcbdc28190d1432894c00d2b1dd25950ffacd96a
Thisura Manjitha Samarakoon <thissamarakoon@gmail.com>
2023-01-02 01:31:20 +0530

    Assets and Record button functionality added

--- VoFX/ViewController.swift
+++ VoFX/ViewController.swift
@@ -10,18 +10,31 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        stopRecordingButton.isEnabled = false
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
     @IBAction func recordAudio(_ sender: Any) {
-        print("Record button was pressed!")
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
     
     @IBAction func stopRecording(_ sender: Any) {
-        print("Stop recording button was pressed!")
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
         recordingLabel.text = "Recording stopped. Tap to Record"
     }
 }
