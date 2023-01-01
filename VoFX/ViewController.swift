b6a6ccd438034473a85962156a670f7f4e2e4084
Thisura Manjitha Samarakoon <thissamarakoon@gmail.com>
2023-01-01 23:55:58 +0530

    Record and stop buttons

--- VoFX/ViewController.swift
+++ VoFX/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record button was pressed!")
+        recordingLabel.text = "Recording in Progress"
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop recording button was pressed!")
+        recordingLabel.text = "Recording stopped. Tap to Record"
+    }
 }
 
